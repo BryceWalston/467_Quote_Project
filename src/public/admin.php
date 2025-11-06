bd53300e726f8b7c033a12cf4a088fdffa107917
Joe Feltz <z2048486@students.niu.edu>
2025-11-06 17:22:39 -0600

    Added dockerfile.dev and docker-compose.yaml to use containers for the project.

---
+++ src/public/admin.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head> 
+        <meta charset="utf-8">
+        <title>Administrator</title>
+    </head>
+    <body>
+        <p>admin page works.</p>
+        <?php
+
+        ?>
+    </body>
+</html>
---
+++ src/public/associate.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head> 
+        <meta charset="utf-8">
+        <title>Sales Associate</title>
+    </head>
+    <body>
+        <p>associate page works.</p>
+        <p>YOOOOOOOOOO!</p>
+        <?php
+            echo "hello world";
+        ?>
+    </body>
+</html>
