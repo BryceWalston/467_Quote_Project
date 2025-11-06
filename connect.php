ee2ae5b4b11728ba5d8f46dc514d62b7c5bf2d7a
Joe Feltz <z2048486@students.niu.edu>
2025-11-06 17:35:33 -0600

    Changed the location of these files, but didn't stage the removal.

--- connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-	$pdo = new PDO('mysql:host=courses;dbname=z1998722', 'z1998722', '2005Jul17');
-	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-	exit("Database connection failed: " . $e->getMessage());
-}
-?>
